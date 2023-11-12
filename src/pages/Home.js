52a7f07a9d47142f9b4229e3aa236254d425d540
David F <davidfawzy02@gmail.com>
2023-11-12 16:06:06 -0500

    Added new project card

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,14 @@ const Home = () => {
 			<Line/>
 			<span className="text">
 				Hello, I am David!<br/><br/>
-				I am a 3rd-year Computer Science student at Brock University. I started programming when I was 12 for fun. So naturally,
+				I am a 4th-year Computer Science student at Brock University. I started programming when I was 12 for fun. So naturally,
 				I am now looking to turn my hobby into a career in software engineering.
 				<br/><br/>
 				I love learning new things, so I worked a little with all kinds of
 				technologies across different fields.
 				<br/><br/>
 				These fields include but are not limited to computer graphics/art, game development,
-				web design, AI, etc. Take a look at my project list! I am always working on expanding it.
+				web design, AI, etc. Take a look at my project list, I am always working on expanding it!
 			</span>
 		</div>
 		<Skills/>
