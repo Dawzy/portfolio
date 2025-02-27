ba37bf0473d30650ae4b75a56fb87009b5108b40
David F <davidfawzy02@gmail.com>
2025-02-27 01:45:28 -0500

    Updated home page!

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -9,14 +9,9 @@ const Home = () => {
 			<Line/>
 			<span className="text">
 				Hello, I am David!<br/><br/>
-				I am a 4th-year Computer Science student at Brock University. I fell in love with software development at an early age, seeking to advance
-        the industry with my passion and curiosity.
+				I am a Computer Science student at Brock University. I have a passion for learning and creating things! Check out my growing projects list!
 				<br/><br/>
-				I love learning new things, so I worked a little with all kinds of
-				technologies across different fields.
-				<br/><br/>
-				These fields include but are not limited to computer graphics/art, game development,
-				web development, and AI. Take a look at my project list, I am always working on expanding it!
+        They range from game development, to AI, to web development and more! Currently, getting my web development skills up 💪
 			</span>
 		</div>
 		<Skills/>
